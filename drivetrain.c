f6c0484b3d3d4245d8989d6e2dee97fa39220b02
geenbeen <43523710+geenbeen@users.noreply.github.com>
2018-09-25 16:01:11 -0500

    Update drivetrain.c

--- drivetrain.c
+++ drivetrain.c
@@ -9,15 +9,17 @@
 //extern int dbgmsk;
 
 void processDriveTrain() {
-
+  //joystick 1, button group 5, button JOY_UP.
   if(joystickGetDigital(1, 5, JOY_UP)) { 
 	  motorSet(WHEEL, 127);
   }
+  //joystick 1, button group 6, button JOY_UP.
   else if(joystickGetDigital(1, 6, JOY_UP)) {
 	  motorSet(WHEEL, -127);
   }
+  //otherwise, null.
   else {
-	  motorSet = 0;
+	  motorSet(WHEEL, 0);
   }
 
 
